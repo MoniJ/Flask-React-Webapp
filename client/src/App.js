f288b6ba9f7de4f37b1078c3a2ab3f80720907c7
MoniJ <monij_93@hotmail.com>
2025-12-17 18:42:05 -0500

    Use flask endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("Loading...");
 
   useEffect(() => {
-    fetch("api")
+    fetch("https://flask-backend-1a0n.onrender.com/api")
       .then(res => res.json())
       .then(data => setMessage(data.message))
       .catch(() => setMessage("⚠️ Failed to fetch backend"));
