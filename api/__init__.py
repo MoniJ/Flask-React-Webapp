e6c9b009cba922baadadd9f4706920d0e2abdfbf
MoniJ <monij_93@hotmail.com>
2025-12-17 23:53:23 -0500

    Add CORS

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,14 @@
 import os
 
-from flask import Flask 
+from flask import Flask
+from flask_cors import CORS
 
 from extensions import db
 from routes import main
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     # postgres://prettyprinted_render_example_user:11vq6k72GmFJazhVpz3pFUko50djVZT1@dpg-ceukdhmn6mpglqdb4avg-a.oregon-postgres.render.com/prettyprinted_render_example
